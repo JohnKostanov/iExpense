645c7bf62d3115ed8163d01897136133ab17cbf8
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2021-12-27 13:13:25 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Джон Костанов on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
