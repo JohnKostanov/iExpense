14390247d3cee91d72f97e3168b12695bdfd7197
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-06-07 22:31:02 +0200

    Added lasted functional in project

---
+++ iExpense/Expenses.swift
@@ -0,0 +1,29 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Джон Костанов on 30/12/21.
+//
+
+import SwiftUI
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+
+        items = []
+    }
+}
